caf85faf2eed8ad3f3afb4df1e2b8ea4a31738ee
cisgenderman <grigorjevsergey1@gmail.com>
2024-10-10 09:26:53 +0700

    Poehali

---
+++ OS_lb_1/src/App.java
@@ -0,0 +1,24 @@
+public class App 
+{
+    /*
+    //Функция-поставщик для Java
+    void synchronized provide()
+    {
+        if (ready)
+        return;
+        ready = true;
+        notify();
+    }
+    //Функция-потребитель для Java
+    void synchronized consume()
+    {
+        while(!ready)
+        wait();
+        ready = false;
+    }
+    */
+    public static void main(String[] args) throws Exception 
+    {
+        System.out.println("Hello, World!");
+    }
+}
