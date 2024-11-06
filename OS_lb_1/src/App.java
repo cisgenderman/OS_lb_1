680b148aa64e0cc947b0a02f510505dea55f130f
cisgenderman <grigorjevsergey1@gmail.com>
2024-11-06 12:07:25 +0700

    Реализовал функции Поставщика и Потребителя

--- OS_lb_1/src/App.java
+++ OS_lb_1/src/App.java
@@ -17,6 +17,40 @@ public class App
         ready = false;
     }
     */
+    
+    //object sinhroniz
+    private static final Object lock = new Object();
+    
+    private static boolean ready = false;
+
+    private static void provide(){
+        synchronized (lock){
+            if (ready)
+                return;
+            //pomeshaem v buffer
+            ready = true;
+            System.out.println("Provided");
+            //uvedomlyaem consumer chto dannie gotovi
+            lock.notify();
+        }
+    }
+
+    private static void consume(){
+        synchronized (lock){
+            while (!ready){
+                try{
+                    lock.wait();
+                    System.out.println("Awoke");
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }        
+            }
+            //potreblyaem dannie
+            ready = false;
+            System.out.println("Consumed");
+        }
+    }
+
     public static void main(String[] args) throws Exception 
     {
         System.out.println("Hello, World!");
